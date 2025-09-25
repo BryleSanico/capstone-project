a26c255e325208023637180ea19f655adf0eb638
BryleSanico <bienebryle.sanico@stratpoint.com>
2025-09-25 08:31:28 +0800

    pre-migraiton

---
+++ ios/ReactViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReactViewController.swift
+//  EventDiscoveryTicketing
+//
+//  Created by Biene Bryle Sanico on 9/24/25.
+//
+
+import UIKit
+
+class ReactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
