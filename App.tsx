4cf883670096fad2d569a7c5f7b8faae39b10807
“BryleSanico” <bienebryle.sanico@stratpoint.com>
2025-10-05 23:43:05 +0800

    fix: moves the hydration of favorites and tickets upon launching the app

--- App.tsx
+++ App.tsx
@@ -1,16 +1,24 @@
-// App.tsx (at the root of your project)
-
-import "react-native-gesture-handler"; // MUST be at the top
+// App.tsx 
+import "react-native-gesture-handler"; 
 import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AppNavigator from "./src/navigation/AppNavigator";
 import SplashScreen from "react-native-splash-screen";
+import { useTickets } from '@/src/hooks/tickets-store';
 
 const queryClient = new QueryClient();
 
 export default function App() {
+
+  const { loadTickets, loadFavorites } = useTickets();
+    // Mount tickets and favorites
+    useEffect (() => {
+      loadTickets();
+      loadFavorites();
+    }, [loadTickets, loadFavorites]);
+
   useEffect(() => {
     SplashScreen.hide();
   }, []);
