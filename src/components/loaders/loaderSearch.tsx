ad06877429d34e0cfd2bba032e2de9e1316663bc
“BryleSanico” <bienebryle.sanico@stratpoint.com>
2025-10-10 17:55:29 +0800

    feat: Added a lottie animation loader and a bash script for the build release preview

---
+++ src/components/loaders/loaderSearch.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+interface LoaderSearchProps {
+  size?: number;
+}
+
+export const LoaderSearch: React.FC<LoaderSearchProps> = ({ size = 100 }) => {
+  return (
+    <View style={[styles.container, { width: size, height: size }]}>
+      <LottieView
+        source={require('../../../assets/lottie-animations/LoaderSearch.json')}
+        autoPlay
+        loop
+        style={{ width: size, height: size }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
