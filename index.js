eb5906640848976a9cda6b60c5734c79ac234255
“BryleSanico” <bienebryle.sanico@stratpoint.com>
2025-11-19 17:02:24 +0800

    fix: register the Firebase background message handler immediately in the entry file.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { getMessaging, setBackgroundMessageHandler } from '@react-native-firebase/messaging';
+import { handleBackgroundMessage } from './src/utils/network/notificationListener';
 
+setBackgroundMessageHandler(getMessaging(), handleBackgroundMessage);
 AppRegistry.registerComponent(appName, () => App);
